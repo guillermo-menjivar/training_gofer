dd5e5df9c2f8cd6549dd97ac9b3f4ba52c67096b
guillermo-menjivar <gmenjivar4@gmail.com>
2025-09-23 17:51:31 -0400

    updates

--- basic_inspection.py
+++ basic_inspection.py
@@ -2,13 +2,20 @@ import json
 
 
 def inspect_relationships(record):
+    source_rendered = ""
+    if record["source_ref"].startwith("intrusion-set"):
+        source_rendered = intrusion_keys[record["source_ref"]]
+
     print(
         f"{record["type"]}: {record["source_ref"]} {record["relationship_type"]} {record["target_ref"]}."
     )
+    print(f"source_rendered: {source_rendered}")
 
 
 metrics = {}
+intrusion_keys = {}
 
+print("we are starting")
 with open("enterprise-attack-17.1.json", "r") as file:
     data = json.load(file)
 
@@ -20,6 +27,8 @@ for obj in objects:
     else:
         metrics[obj["type"]] = metrics.get(obj["type"]) + 1
 
+    if obj["type"] == "intrusion-set":
+        intrusion_keys[obj["id"]] = obj
+
     if obj["type"] == "relationship":
         inspect_relationships(obj)
-print(metrics)
